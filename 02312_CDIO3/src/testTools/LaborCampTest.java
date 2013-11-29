95d0122386c00788c587c4dc036f43f551d803f3
tmmortensen <thomasmortensen@Thomass-MacBook-Air.local>
2013-11-29 12:34:07 +0100

    Alle testklasser oprettet.

---
+++ 02312_CDIO3/src/testTools/LaborCampTest.java
@@ -0,0 +1,112 @@
+package testTools;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import entity.Field;
+import entity.Player;
+import entity.Tax;
+
+public class LaborCampTest {
+	private Player player;
+	private Field tax200;
+	private Field tax0;
+	private Field taxNeg200;
+
+	@Before
+	public void setUp() throws Exception {
+		this.player = new Player(1000, "Anders And");
+		this.tax200 = new Tax("Helle +200", 200);
+		this.tax0 = new Tax("Helle 0", 0);
+		this.taxNeg200 = new Tax("Helle -200", -200);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		this.player = new Player(1000, "Anders And");
+		// The fields are unaltered
+	}
+
+	@Test
+	public void testEntities() {
+		Assert.assertNotNull(this.player);
+		Assert.assertNotNull(this.tax200);
+		Assert.assertNotNull(this.tax0);
+		Assert.assertNotNull(this.taxNeg200);
+		Assert.assertTrue(this.tax200 instanceof Tax);
+		Assert.assertTrue(this.tax0 instanceof Tax);
+		Assert.assertTrue(this.taxNeg200 instanceof Tax);
+	}
+
+	@Test
+	public void testLandOnField200() {
+		int expected = 1000;
+		int actual = this.player.getAccountValue();
+		Assert.assertEquals(expected, actual);
+
+		// Perform the action to be tested
+		this.tax200.landOnField(this.player);
+		expected = 1000 + 200;
+		actual = this.player.getAccountValue();
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testLandOnField200Twice() {
+
+		int expected = 1000;
+		int actual = this.player.getAccountValue();
+		Assert.assertEquals(expected, actual);
+
+		// Perform the action to be tested
+		this.tax200.landOnField(this.player);
+		this.tax200.landOnField(this.player);
+		expected = 1000 + 200 + 200;
+		actual = this.player.getAccountValue();
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testLandOnField0() {
+		int expected = 1000;
+		int actual = this.player.getAccountValue();
+		Assert.assertEquals(expected, actual);
+
+		// Perform the action to be tested
+		this.tax0.landOnField(this.player);
+		expected = 1000;
+		actual = this.player.getAccountValue();
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testLandOnField0Twice() {
+		int expected = 1000;
+		int actual = this.player.getAccountValue();
+		Assert.assertEquals(expected, actual);
+
+		// Perform the action to be tested
+		this.tax0.landOnField(this.player);
+		this.tax0.landOnField(this.player);
+		expected = 1000;
+		actual = this.player.getAccountValue();
+		Assert.assertEquals(expected, actual);
+	}
+
+	// @Test
+	// public void testLandOnFieldNeg200() {
+	// int expected = 1000;
+	// int actual = this.player.getAccountValue();
+	// Assert.assertEquals(expected, actual);
+	//
+	// // Perform the action to be tested
+	// this.refugeNeg200.landOnField(this.player);
+	//
+	// // It is not possible to deposit a negative amount
+	// expected = 1000;
+	// actual = this.player.getAccountValue();
+	// Assert.assertEquals(expected, actual);
+	// }
+}
---
+++ 02312_CDIO3/src/testTools/TerritoryTest.java
@@ -0,0 +1,115 @@
+package testTools;
+
+import java.util.Scanner;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import entity.Field;
+import entity.Player;
+import entity.Territory;
+
+public class TerritoryTest {
+	private Player player;
+	private Field ter200;
+	private Field ter0;
+	private Field terNeg200;
+	Scanner scanner = new Scanner(System.in);
+
+	@Before
+	public void setUp() throws Exception {
+		this.player = new Player(1000, "Anders And");
+		this.ter200 = new Territory("Helle +200", 200, 200, scanner);
+		this.ter0 = new Territory("Helle 0", 0, 0, scanner);
+		this.terNeg200 = new Territory("Helle -200", -200, -200, scanner);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		this.player = new Player(1000, "Anders And");
+		// The fields are unaltered
+	}
+
+	@Test
+	public void testEntities() {
+		Assert.assertNotNull(this.player);
+		Assert.assertNotNull(this.ter200);
+		Assert.assertNotNull(this.ter0);
+		Assert.assertNotNull(this.terNeg200);
+		Assert.assertTrue(this.ter200 instanceof Territory);
+		Assert.assertTrue(this.ter0 instanceof Territory);
+		Assert.assertTrue(this.terNeg200 instanceof Territory);
+	}
+
+	@Test
+	public void testLandOnField200() {
+		int expected = 1000;
+		int actual = this.player.getAccountValue();
+		Assert.assertEquals(expected, actual);
+
+		// Perform the action to be tested
+		this.ter200.landOnField(this.player);
+		expected = 1000 + 200;
+		actual = this.player.getAccountValue();
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testLandOnField200Twice() {
+
+		int expected = 1000;
+		int actual = this.player.getAccountValue();
+		Assert.assertEquals(expected, actual);
+
+		// Perform the action to be tested
+		this.ter200.landOnField(this.player);
+		this.ter200.landOnField(this.player);
+		expected = 1000 + 200 + 200;
+		actual = this.player.getAccountValue();
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testLandOnField0() {
+		int expected = 1000;
+		int actual = this.player.getAccountValue();
+		Assert.assertEquals(expected, actual);
+
+		// Perform the action to be tested
+		this.ter0.landOnField(this.player);
+		expected = 1000;
+		actual = this.player.getAccountValue();
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testLandOnField0Twice() {
+		int expected = 1000;
+		int actual = this.player.getAccountValue();
+		Assert.assertEquals(expected, actual);
+
+		// Perform the action to be tested
+		this.ter0.landOnField(this.player);
+		this.ter0.landOnField(this.player);
+		expected = 1000;
+		actual = this.player.getAccountValue();
+		Assert.assertEquals(expected, actual);
+	}
+
+	// @Test
+	// public void testLandOnFieldNeg200() {
+	// int expected = 1000;
+	// int actual = this.player.getAccountValue();
+	// Assert.assertEquals(expected, actual);
+	//
+	// // Perform the action to be tested
+	// this.refugeNeg200.landOnField(this.player);
+	//
+	// // It is not possible to deposit a negative amount
+	// expected = 1000;
+	// actual = this.player.getAccountValue();
+	// Assert.assertEquals(expected, actual);
+	// }
+}
